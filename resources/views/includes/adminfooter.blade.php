919ca894d7a0bc9187e08b731c41ea18f5b9453f
Omer <omer.ebubekir38@gmail.com>
2022-12-15 23:30:34 +0300

    Improve System

--- resources/views/includes/adminfooter.blade.php
+++ resources/views/includes/adminfooter.blade.php
@@ -1,11 +1,9 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
     <div class="copyright">
-        &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Dilekçe Sepeti</span></strong>
     </div>
-    <div class="credits">
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-    </div>
+    <div class="credits"><a href="https://pendikyazilim.com/">Pendik Yazılım</a></div>
 </footer><!-- End Footer -->
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
