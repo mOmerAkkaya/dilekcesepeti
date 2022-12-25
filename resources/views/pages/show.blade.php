c8f726c8870581f9d609d3e29885265ec9731b0c
Omer <omer.ebubekir38@gmail.com>
2022-12-25 21:15:58 +0300

    detail change

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -88,15 +88,16 @@
                         </div>
                     </form>
                 </div>
-            </div>
-            <div class="card">
-                <h5 class="card-header">Featured</h5>
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="card">
+                    <h5 class="card-header">Featured</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
             </div>
+
 
         </div>
     </section><!-- End Service Details Section -->
