6f024c51bce68e9be2c66e07e5dbaa82ba09798e
Ömer <omer.ebubekir38@gmail.com>
2022-12-29 00:26:55 +0300

    comment

--- resources/views/pages/finish.blade.php
+++ resources/views/pages/finish.blade.php
@@ -121,19 +121,7 @@
                 </form>
                 </p>
                 <p>
-                <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card h-100">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
+                <div class="row row-cols-1 row-cols-md-1 g-4">
   <div class="col">
     <div class="card h-100">
       <img src="..." class="card-img-top" alt="...">
