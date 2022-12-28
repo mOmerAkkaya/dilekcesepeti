188ba973518bb09eb7551a9cd66919d2f625215a
Ömer <omer.ebubekir38@gmail.com>
2022-12-29 00:48:41 +0300

    empty fix

--- resources/views/pages/finish.blade.php
+++ resources/views/pages/finish.blade.php
@@ -121,7 +121,7 @@
                 </form>
                 </p>
                 <p>
-                @forelse ($data->comments as $key => $value)
+                @forelse ($data->comments as $value)
                 <div class="row row-cols-1 row-cols-md-1 g-4">
                 <div class="col">
                     <div class="card h-100">
