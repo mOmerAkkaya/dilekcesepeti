aa47be3a3ec7c14366d4d6dc69f031b26237ff1f
Omer <omer.ebubekir38@gmail.com>
2023-01-01 09:43:12 +0300

    comments model

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Comments extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 
     public function user(){
         return $this->hasMany('\App\Models\User', 'id', 'user_id');
