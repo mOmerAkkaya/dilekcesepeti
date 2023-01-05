148546892958461fb6f117887b73b3e7b1fc2d0d
Omer <omer.ebubekir38@gmail.com>
2023-01-05 09:51:07 +0300

    Data

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -37,7 +37,7 @@ $value = json_decode($value);
             <div class="container">
                 <ol>
                     <li><a href="{{route('index')}}">{{ __('menu.home') }}</a></li>
-                    <li>{{$value[$data->doc_type]}}</li>
+                    <li>{{$value}}</li>
                     
                 </ol>
             </div>
