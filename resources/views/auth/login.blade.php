ed31d318b3f7f78c6b19106b7b5a4fb5aedca330
Omer <omer.ebubekir38@gmail.com>
2023-01-14 22:22:06 +0300

    login fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,62 +21,62 @@
 </head>
 
 <body>
-<main class="d-flex w-100">
-    <div class="container d-flex flex-column">
-        <div class="row vh-100">
-            <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
-                <div class="d-table-cell align-middle">
+    <main class="d-flex w-100">
+        <div class="container d-flex flex-column">
+            <div class="row vh-100">
+                <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+                    <div class="d-table-cell align-middle">
 
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="m-sm-4">
-                                <div class="text-center">
-                                    <img src="{{asset('img/dilekce.jpg')}}" alt="Dilekçe Sepeti" class="img-fluid" width="132" height="132" />
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="m-sm-4">
+                                    <div class="text-center">
+                                        <img src="{{asset('img/dilekce.jpg')}}" alt="Dilekçe Sepeti" class="img-fluid" width="132" height="132" />
+                                    </div>
+                                    <form action="{{route('login')}}" method="post">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label class="form-label">E-Posta</label>
+                                            <input class="form-control form-control-lg" type="email" name="email" placeholder="E-Posta Adresiniz" />
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Şifreniz</label>
+                                            <input class="form-control form-control-lg" type="password" name="password" placeholder="Şifrenizi Giriniz" />
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                            <small>
+                                                <a href="{{ route('register') }}">Şifremi unuttum</a>
+                                            </small>
+                                        </div>
+                                        <div>
+                                            <label class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="remember-me" name="remember-me" checked>
+                                                <span class="form-check-label">Beni hatırla</span>
+                                            </label>
+                                        </div>
+                                        <div class="text-center mt-3">
+                                            <button type="submit" class="btn btn-lg btn-primary">Giriş Yap</button>
+                                        </div>
+                                    </form>
                                 </div>
-                                <form action="{{route('login')}}" method="post">
-                                    @csrf
-                                    <div class="mb-3">
-                                        <label class="form-label">E-Posta</label>
-                                        <input class="form-control form-control-lg" type="email" name="email" placeholder="E-Posta Adresiniz" />
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label">Şifreniz</label>
-                                        <input class="form-control form-control-lg" type="password" name="password" placeholder="Şifrenizi Giriniz" />
-                                        @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
-                                        <small>
-                                            <a href="index.html">Şifremi unuttum</a>
-                                        </small>
-                                    </div>
-                                    <div>
-                                        <label class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="remember-me" name="remember-me" checked>
-                                            <span class="form-check-label">Beni hatırla</span>
-                                        </label>
-                                    </div>
-                                    <div class="text-center mt-3">
-                                        <button type="submit" class="btn btn-lg btn-primary">Giriş Yap</button>
-                                    </div>
-                                </form>
                             </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 
-<script src="{{asset('admin/js/app.js')}}"></script>
+    <script src="{{asset('admin/js/app.js')}}"></script>
 
 </body>
 
