700b1525c0626d0966cc7a12423881b84c841214
Omer <omer.ebubekir38@gmail.com>
2023-01-17 12:20:32 +0300

    twitter meta card

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -15,6 +15,13 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
+<!-- Twitter Card -->
+<meta name="twitter:card" content="https://dilekcesepeti.com.tr/img/dilekce.jpg" />
+<meta name="twitter:title" content="@yield('title', $page->description)" />
+<meta name="twitter:description" content="@yield('title', $page->description)" />
+<meta name="twitter:url" content="{{url()->full()}}" />
+<meta name="twitter:image" content="https://dilekcesepeti.com.tr/img/dilekce.jpg" />
+
 <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
 <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
