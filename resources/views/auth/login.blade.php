2548dbfb31465fba00394156dfcf8440c246fcb6
Omer <omer.ebubekir38@gmail.com>
2023-01-23 10:25:04 +0300

    login description

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
-    <meta name="author" content="AdminKit">
-    <meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+    <meta name="description" content="Giriş | Dilekçe Sepeti | dilekcesepeti.com.tr">
+    <meta name="author" content="Pendik Yazılım">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="{{asset('admin/img/icons/icon-48x48.png')}}" />
